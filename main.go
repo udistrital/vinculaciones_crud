b42d5a4d0f888aa1b2ab53474233efb3a316c80c
Alejandra Ortiz <mariaalejandraortiz0401@gmail.com>
2019-12-31 13:08:52 -0500

    @AlaskaRising, se añaden los archivos necesarios para despliegue. Se modifica main para insertar cabeceras. Se crea swagger y se añaden archivos de gitignore. Se modifican variables de entorno #2

--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	_ "github.com/udistrital/vinculaciones_crud/routers"
-
+	"github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+	"github.com/astaxie/beego/plugins/cors"
+	"github.com/udistrital/utils_oas/customerror"
+	
 )
 
-func main() {
+func test() {
 	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+}
+
+func init() {
+	test()
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+}
+
+func main() {
+	
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+	apistatus.Init()  
+	beego.ErrorController(&customerror.CustomErrorController{})
 	beego.Run()
 }
